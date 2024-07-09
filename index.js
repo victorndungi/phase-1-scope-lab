e6815249f2287e503051a95a6b291632fcca0cf0
victorndungi <victor.ndungi@student.moringaschool.com>
2024-07-09 21:06:55 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+
+window.customerName = 'bob';
+
+const leastFavoriteCustomer = 'someone'
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    window.bestCustomer='not bob'
+}
+
+function overwriteBestCustomer(newBestCustomer){
+    window.bestCustomer=newBestCustomer
+}
+
+function changeLeastFavoriteCustomer() {
+    
+    leastFavoriteCustomer = 'maybe someone else';
+}
